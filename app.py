7126d4f49e924e072d3e567bf341b4d19df343d0
Shinsuke Kawahara <kwhrs0811@icloud.com>
2025-05-07 02:29:27 +0900

    ステップ1:Flask初期構成とindexページ追加

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from flask import render_template
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
